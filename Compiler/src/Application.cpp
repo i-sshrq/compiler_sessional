1f4d27aaef52766bf3529b95e1bdb854136299bc
i-sshrq <i_ssh_rq@protonmail.com>
2020-02-08 15:14:23 +0600

    Visual Studio addition

---
+++ Compiler/src/Application.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "SymbolTable.h"
+using namespace std;
+int main() {
+	SymbolTable s1;
+}
---
+++ Compiler/src/SymbolTable.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <list>
+
+class SymbolInfo {
+
+};
+
+
+class SymbolTable {
+private:
+	
+public:
+	SymbolTable() {
+
+	}
+};
