5aa2c39797991c6552d03cb263cf3006100fd5a5
Ishraq <i_ssh_rq@protonmail.com>
2020-02-08 20:40:04 +0600

    insert and print tokens into table

--- Compiler/src/Application.cpp
+++ Compiler/src/Application.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "SymbolTable.h"
 using namespace std;
+
 int main() {
-	SymbolTable s1;
+	SymbolTable * s1 = new SymbolTable();
+	s1->insert_token("123", "NUMBER");
+	s1->insert_token("231", "NUMBER");
+	s1->insert_token("foo", "FUNCTION");
+	s1->print_tokens();
+
 }
--- Compiler/src/SymbolTable.h
+++ Compiler/src/SymbolTable.h
@@ -2,16 +2,72 @@
 #include <string>
 #include <list>
 
-class SymbolInfo {
+using namespace std;
 
+//Hash function for token insertion
+int hashFunc(string name) {
+	int length = name.length();
+	if (length == 1)
+		return (name[0] * 23) % 101;
+	else if (length % 2 == 0)
+		return ((name[length - 2] + name[length - 1])*23)%101;
+	else
+		return ((name[0] + name[1]) * 23) % 101;
+}
+
+
+class SymbolInfo {
+private:
+	string symbol;
+	string type;
+public:
+	SymbolInfo(string sym, string typ) {
+		symbol = sym;
+		type = typ;
+	}
+	string getSymbol() {
+		return symbol;
+	}
+	string getType() {
+		return type;
+	}
 };
 
 
 class SymbolTable {
 private:
-	
+	list<SymbolInfo>* table;
 public:
-	SymbolTable() {
 
+	//Constructors and destructors for the table
+	SymbolTable() {
+		table = new list<SymbolInfo>[101];
 	}
+	~SymbolTable() {
+		for (int i = 0; i < 101; i++)
+			table[i].clear();
+	}
+
+	//Insert each token into the proper place in the table
+	void insert_token(string sym, string typ) {
+		/// TODO confusion
+		int key = hashFunc(typ);
+		table[key].push_back(SymbolInfo(sym, typ));
+	}
+
+	//Prints the entire table
+	void print_tokens() {
+		for (int i = 0; i < 101; i++) {
+			cout << i << " ------> ";
+			for (list<SymbolInfo>::iterator j = table[i].begin(); j != table[i].end(); j++) {
+				string tempSym = (*j).getSymbol();
+				string tempType = (*j).getType();
+				cout << "<" << tempSym << ", " << tempType << ">    ";
+			}
+			cout << endl;
+		}
+	}
+
+
+	
 };
