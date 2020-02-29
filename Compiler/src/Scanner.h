be3e442d05b560d00b807669137dd8d7b9cd88fd
Ishraq <i_ssh_rq@protonmail.com>
2020-02-29 20:54:55 +0600

    Finished Filtering Keywords

--- Compiler/src/Scanner.h
+++ Compiler/src/Scanner.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "SymbolTable.h"
+#include <stack>
 #include <fstream>
 
 class Scanner {
@@ -17,6 +18,48 @@ public:
 		s1 = s;
 		inputPath = srcPath;
 	}
+	//Returns the matched keyword or empty string
+	string keywordChecker(string token, string otherLeft, string otherRight) {
+		vector<string> actualKeywords= {"if", "else", "int", "for", "while", "break", "continue", "float", "double", "char", "return"};
+		for (int i = 0; i < actualKeywords.size(); i++) {
+			size_t pos = token.find(actualKeywords[i]);
+			if (pos != string::npos) {
+				//on perfect match
+				if (token == actualKeywords[i])
+					return token;
+				//found, but look for nearby characters RIGHT
+				if (pos == 0) {
+					for (int j = 0; j < otherRight.length(); j++) {
+						if (token[actualKeywords[i].size()] == otherRight[j])
+							return actualKeywords[i];
+					}
+				}
+				//found, but look for nearby characters LEFT
+				else {
+					//cout << "POS = " << pos << endl;
+					for (int j = 0; j < otherLeft.length(); j++) {
+						if (token[pos - 1] == otherLeft[j])
+							return actualKeywords[i];
+					}
+				}
+			}
+		}
+		return string("");
+	}
+
+	string functionChecker(string token, string otherLeft, string otherRight) {
+		//Check functions by parenthesis matching
+		stack <char> stack_p;
+		string actualKeywords[] = { "if", "for", "while", "else if" };
+
+
+
+	}
+
+
+
+
+
 	void assignment1() {
 	
 
@@ -82,26 +125,75 @@ public:
 	}
 
 	void filterKeywords() {
-		vector <string> wordBuffer;
+		//vector <string> wordBuffer;
 		ifstream inputFile(inputPath);
 		string line;
 		char delimiters[] = " \t\n";
 		char* token;
 		char* next_token = NULL;
+		int lineNumber = 0;
+		vector< pair<string, int> > keywords;
 		if (inputFile.is_open()) {
 			while (getline(inputFile, line)) {
+				lineNumber++;
 				token = strtok_s((char*)line.c_str(), delimiters, &next_token);
 				while (token){
-					wordBuffer.push_back(string(token));
+					//Parse the token
+					string otherRight = "(),;";
+					string otherLeft = "(),";
+					string temp = keywordChecker(token, otherLeft, otherRight);
+					if (temp.length() != 0) {
+						//cout << "FOUND " << temp << endl;
+						keywords.push_back(make_pair(temp, lineNumber));
+					}					
 					token = strtok_s(NULL, delimiters, &next_token);
 				}
 			}
-		}
-		for (int i = 0; i < wordBuffer.size(); i++) {
-			cout << wordBuffer[i];
+			inputFile.close();
 		}
 
+		
 
+		//Tokenized, output to file in tsv
+		ofstream outFile("output_keyword.txt", ofstream::out);
+		for (int i = 0; i < keywords.size(); i++) {
+			outFile << keywords[i].first << "\t" << keywords[i].second << endl;
+		}
+		outFile.close();
 	}
+
+	void filterFunctions() {
+		ifstream inputFile(inputPath);
+		string line;
+		int lineNumber = 0;
+		char * token;
+		char * next_token = NULL;
+		char delimiters[] = " \t\n";
+
+		vector < pair<string, int> > functions;
+
+		if (inputFile.is_open()) {
+			while (getline(inputFile, line)) {
+				int pos = line.find("(");
+				if (pos != string::npos) {
+					lineNumber++;
+					token = strtok_s((char*)line.c_str(), delimiters, &next_token);
+					while (token) {
+						//Parse the token
+						string otherRight = "},";
+						string otherLeft = "{,";
+						string temp = functionChecker(token, otherLeft, otherRight);
+						if (temp.length() != 0) {
+							functions.push_back(make_pair(temp, lineNumber));
+						}
+						token = strtok_s(NULL, delimiters, &next_token);
+					}
+				}
+			}
+
+			inputFile.close();
+		}
+	}
+	
 
 };
